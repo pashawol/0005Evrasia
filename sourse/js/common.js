05457de52af6325f240ac279ef9268c6dbb3b894
Pavel <wol1414@gmail.com>
2020-07-25 01:33:53 +0300

    start

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -47,242 +47,56 @@ const JSCCommon = {
 			modal.find('.order').val(content.order);
 		})
 	},
-	// /magnificPopupCall
-	toggleMenu() {
-		let _this = this;
-		if (_this.btnToggleMenuMobile) {
-
-			_this.btnToggleMenuMobile.forEach(function (element) {
-				element.addEventListener('click', function () {
-
-					_this.btnToggleMenuMobile.forEach(function (element) {
-						element.classList.toggle("on");
-					});
-					_this.menuMobile.classList.toggle("active");
-					_this.body.classList.toggle("fixed");
-
-					return false;
-				});
-			});
-		}
-	},
-
-	closeMenu() {
-		let _this = this;
-		if (_this.menuMobile) {
-
-			_this.btnToggleMenuMobile.forEach(function (element) {
-				element.classList.remove("on");
-
-			});
-			_this.menuMobile.classList.remove("active");
-			_this.body.classList.remove("fixed");
-		}
-
-	},
-
-	mobileMenu() {
-		// закрыть/открыть мобильное меню
-		let _this = this;
-		if (_this.menuMobileLink) {
-
-			_this.toggleMenu();
-			// _this.menuMobileLink.forEach(function (element) {
-			// 	element.addEventListener('click', function (e) {
-			// 		console.log(element);
-			// 		_this.closeMenu();
-
-			// 	});
-			// })
-			document.addEventListener('mouseup', function (event) {
-				let container = event.target.closest(".menu-mobile--js.active"); // (1)
-				if (!container) {
-					_this.closeMenu();
-
-				}
-			}, { passive: true });
-		}
-	},
-	// /mobileMenu
-
-	// табы  . 
-	tabscostume(tab) {
-		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
-			$(this)
-				.addClass('active').siblings().removeClass('active')
-				.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-				.eq($(this).index()).show().addClass('active');
-
-		});
-	},
-	// /табы  
-	inputMask() {
-		// mask for input
-		$('input[type="tel"]').attr("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask("+9(999)999-99-99");
-	}
-	// /inputMask
-
+  
 };
 
 function eventHandler() { 
 	JSCCommon.modalCall();
-
-	JSCCommon.tabscostume('tabs');
-
-	JSCCommon.mobileMenu();
-
-	JSCCommon.inputMask();
-
-	// JSCCommon.CustomInputFile();
-	// добавляет подложку для pixel perfect
-	$(".main-wrapper").after('<div class="pixel-perfect" style="background-image: url(screen/main.jpg);"></div>')
-	// /добавляет подложку для pixel perfect
-
-
-
-	// const url = document.location.href;
-	// $.each($(".top-nav__nav a "), function() {
-
-	// 	if (this.href == url) {
-	// 		if ($(this).hasClass("top-nav__link") == true) {
-
-	// 			$(this).addClass('top-nav__link-active');
-	// 		}
-	// 		if ($(this).hasClass("footer__link") == true) {
-
-	// 			$(this).addClass('footer__link-active');
-	// 		} 
-	// 	}; 
-	// }); 
-
-	// /закрыть/открыть мобильное меню
-
-	function heightses() {
- 
-		// скрывает моб меню
-
-		const topH = document.querySelector('header').scrollHeight;
-		let stickyElement = document.querySelector('.top-nav')
-		window.onscroll = () => {
-			if ($(window).scrollTop() > topH) {
-
-				stickyElement.classList.add('fixed');
-			} else {
-				stickyElement.classList.remove('fixed'); 
-			}
-		};
-		// конец добавил
-		if (window.matchMedia("(min-width: 992px)").matches) {
-			JSCCommon.closeMenu();
-		}
-	}
-
-	window.addEventListener('resize', () => {
-		heightses();
-
-	});
-
-	heightses();
-
-	// листалка по стр
-	$(" .top-nav li a, .scroll-link").click(function () {
-		const elementClick = $(this).attr("href");
-		const destination = $(elementClick).offset().top;
-
-		$('html, body').animate({ scrollTop: destination }, 1100);
-
-		return false;
-	});
-
+  
+  
 	let defaultSl = {
-
-	}
-	const swiper4 = new Swiper('.color-slider', {
-		// slidesPerView: 5,
-		...defaultSl,
-		slidesPerView: 'auto',
+		spaceBetween: 0,
+		lazy: {
+			loadPrevNext: true,
+			loadPrevNextAmount: 6,
+		},
 		watchOverflow: true,
 		spaceBetween: 0,
-		freeMode: true,
-		watchOverflow: true,
-		slidesPerGroup: 3,
-
-		// centeredSlides: true,
 		loop: true,
-		loopFillGroupWithBlank: true,
-		touchRatio: 0.2,
-		slideToClickedSlide: true,
-		freeModeMomentum: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
+		
+	}
+	
+	const swiper4 = new Swiper('.sGal__slider--js', {
+		// slidesPerView: 5,
+		...defaultSl,
+		effect: 'coverflow',
+		spaceBetween: 0,
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		coverflowEffect: {
+			rotate: 0,
+			// stretch: -60,
+			depth: 350,
+			modifier: 1,
+			slideShadows: false,
 		},
 
 	});
 	// modal window
-
- 
-
-	var gets = (function () {
-		var a = window.location.search;
-		var b = new Object();
-		var c;
-		a = a.substring(1).split("&");
-		for (var i = 0; i < a.length; i++) {
-			c = a[i].split("=");
-			b[c[0]] = c[1];
-		}
-		return b;
-	})();
-	// form
-	$("form").submit(function (e) {
-		e.preventDefault();
-		const th = $(this);
-		var data = th.serialize();
-		th.find('.utm_source').val(decodeURIComponent(gets['utm_source'] || ''));
-		th.find('.utm_term').val(decodeURIComponent(gets['utm_term'] || ''));
-		th.find('.utm_medium').val(decodeURIComponent(gets['utm_medium'] || ''));
-		th.find('.utm_campaign').val(decodeURIComponent(gets['utm_campaign'] || ''));
-		$.ajax({
-			url: 'action.php',
-			type: 'POST',
-			data: data,
-		}).done(function (data) {
-
-			$.fancybox.close();
-			$.fancybox.open({
-				src: '#modal-thanks',
-				type: 'inline'
-			});
-			// window.location.replace("/thanks.html");
-			setTimeout(function () {
-				// Done Functions
-				th.trigger("reset");
-				// $.magnificPopup.close();
-				// ym(53383120, 'reachGoal', 'zakaz');
-				// yaCounter55828534.reachGoal('zakaz');
-			}, 4000);
-		}).fail(function () { });
-
+	var wow = new WOW({
+		mobile: false,
+		animateClass: 'animate__animated',
 	});
+	wow.init();
+  
 
 	var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
 	if (isIE11) {
 		$("body").prepend(`<p   class="browsehappy container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p>`)
 
 	}
-
-	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-	let vh = window.innerHeight * 0.01;
-	// Then we set the value in the --vh custom property to the root of the document
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-	// We listen to the resize event
-	window.addEventListener('resize', () => {
-		// We execute the same script as before
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	}, { passive: true });
+ 
 };
 if (document.readyState !== 'loading') {
 	eventHandler();
---
+++ sourse/pug/blocks/sDay/_sDay.js
@@ -0,0 +1,13 @@
+let sDayVue = new Vue({
+		el: '#sDay',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
